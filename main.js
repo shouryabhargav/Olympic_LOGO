d219e46c07052445240fde55c9837bffa4c42f9c
Shourya Bhargava <80098640+shouryabhargav@users.noreply.github.com>
2021-05-31 23:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var mycanvas=document.getElementById("mycanvas");      
+var anyvariablename= mycanvas.getContext("2d");
+
+color = "blue";
+anyvariablename.beginPath();
+anyvariablename.strokeStyle = color;
+anyvariablename.lineWidth = 4;
+anyvariablename.arc(150, 143, 50 ,0 , 2*Math.PI);
+anyvariablename.stroke();
+
+color1 = "yellow";
+anyvariablename.beginPath();
+anyvariablename.strokeStyle = color1;
+anyvariablename.lineWidth = 4;
+anyvariablename.arc(205, 180, 50 ,0 , 2*Math.PI);
+anyvariablename.stroke();
+
+color2 = "black";
+anyvariablename.beginPath();
+anyvariablename.strokeStyle = color2;
+anyvariablename.lineWidth = 4;
+anyvariablename.arc(260, 143, 50 ,0 , 2*Math.PI);
+anyvariablename.stroke();
+
+color1 = "green";
+anyvariablename.beginPath();
+anyvariablename.strokeStyle = color1;
+anyvariablename.lineWidth = 4;
+anyvariablename.arc(315, 180, 50 ,0 , 2*Math.PI);
+anyvariablename.stroke();
+
+color = "red";
+anyvariablename.beginPath();
+anyvariablename.strokeStyle = color;
+anyvariablename.lineWidth = 4;
+anyvariablename.arc(370, 143 , 50 ,0 , 2*Math.PI);
+anyvariablename.stroke();
+
+
+
